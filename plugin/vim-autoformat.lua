1540efa4a3a92827a1f26c8c5663e725d86808db
Marcio Lima <Superar@users.noreply.github.com>
2023-05-28 20:59:03 +0100

    Add autoformat config for Windows

--- plugin/vim-autoformat.lua
+++ plugin/vim-autoformat.lua
@@ -1,1 +1,5 @@
-vim.g.python3_host_prog = '/usr/bin/python3'
+if jit.os == 'Windows' then
+    vim.g.python3_host_prog = 'D:\\Programas\\Python310\\python.exe'
+else
+    vim.g.python3_host_prog = '/usr/bin/python3'
+end
