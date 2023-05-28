dab89ec4399a970f8172ce318daa409103850e71
Marcio Lima <Superar@users.noreply.github.com>
2023-05-28 20:56:59 +0100

    Add vimtex Windows viewer

--- plugin/vimtex.lua
+++ plugin/vimtex.lua
@@ -1,4 +1,9 @@
 -- -------- LaTeX options --------
-vim.g.vimtex_view_method = 'zathura'
+if jit.os == 'Windows' then
+    vim.g.vimtex_view_general_viewer = 'SumatraPDF'
+    vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
+else
+    vim.g.vimtex_view_method = 'zathura'
+end
 vim.g.vimtex_quickfix_mode = 1
 vim.g.vimtex_quickfix_open_on_warning = 0
